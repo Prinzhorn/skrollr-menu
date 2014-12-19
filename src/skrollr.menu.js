87c0bdb32cf5df73920d2ea2c863cb2d28c5960a
Mickael Desgranges <mickael.desgranges@gmail.com>
2014-12-19 09:14:48 +0100

    let me a chance to know if the hash has changed, with or without supportsHistory

--- src/skrollr.menu.js
+++ src/skrollr.menu.js
@@ -131,11 +131,12 @@
 
 		if(supportsHistory && !fake) {
 			history.pushState({top: targetTop}, '', hash);
-			// please, let me a chance to know if the hash has changed
-			if ( typeof _skrollrInstance.onHashChange === 'function' ) {
-                            _skrollrInstance.onHashChange(hash);
-                        }
 		}
+		
+		// please, let me a chance to know if the hash has changed
+		if ( typeof _skrollrInstance.onHashChange === 'function' ) {
+                        _skrollrInstance.onHashChange(hash);
+                }
 
 		var menuDuration = parseInt(link.getAttribute(MENU_DURATION_ATTR), 10);
 		var animationDuration = _duration(_skrollrInstance.getScrollTop(), targetTop);
